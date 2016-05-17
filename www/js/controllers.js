2272d78264227cd3714a84b44dcdadfbf77bc445
Diogo Parrinha <piratanervo@gmail.com>
2016-05-17 16:04:19 +0200

    Added "Browse" page
    It's the home page of the app for now.
    Shows connection suggestions.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,19 @@
 angular.module('meetabroad.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, auth, ApiData) {
+.controller('AppCtrl', function($scope, $ionicPopup, $timeout, auth, ApiData) {
 
 	$scope.logOut = auth.logOut;
 	$scope.loggedIn = auth.isLoggedIn;
 	
 	$scope.ApiUrl = ApiData.url;
+	
+	// An alert dialog
+	$scope.showAlert = function(message) {
+		var alertPopup = $ionicPopup.alert({
+			title: 'Error',
+			template: message,
+		});
+	};
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -15,22 +23,14 @@ angular.module('meetabroad.controllers', [])
   //});
 })
 
-.controller('LoginController', function($scope, $state, auth, $ionicPopup, $timeout, $window) {
+.controller('LoginController', function($scope, $state, auth, $window) {
     $scope.user = {};
 
-	// An alert dialog
-	var showAlert = function(message) {
-		var alertPopup = $ionicPopup.alert({
-			title: 'Error',
-			template: message,
-		});
-	};
-	
     $scope.logIn = function(){
       auth.logIn($scope.user).error(function(data){
         $scope.error = data;
 
-		showAlert($scope.error.message);
+		$scope.showAlert($scope.error.message);
 		  
       }).then(function(){
 		  
@@ -56,18 +56,10 @@ angular.module('meetabroad.controllers', [])
 .controller('BrowseController', function($scope, $http, ApiData, auth) {
 	$scope.suggestions = [];
 	
-	auth.getUser().then(function(response){
+	$scope.loadSuggestions = function(u){
 		
-		user = response.data;
-		
-		$http.get(ApiData.url+'/interests'/*, {
-			headers: {Authorization: 'Bearer '+auth.getToken()}
-		}*/).then(function(response){
-			data = response.data;
+		var u = user;
 
-			$scope.interests = data;
-		});
-		
 		// Make suggestions available to the whole app
 		$http.get(ApiData.url+'/users/destinationcity/'+user.destinationcountry+'/'+user.destinationcity, {
 			headers: {Authorization: 'Bearer '+auth.getToken()}
@@ -88,6 +80,7 @@ angular.module('meetabroad.controllers', [])
 					// Remove them
 					for (var i = suggestions.length - 1; i >= 0; i--) {
 						var s = suggestions[i];
+						s.loading = false;
 						
 						for (var j = connections.length - 1; j >= 0; j--) {
 							var c = connections[j];
@@ -106,12 +99,43 @@ angular.module('meetabroad.controllers', [])
 					
 					// We have our results now
 					$scope.suggestions = suggestions;
+					$scope.$broadcast('scroll.refreshComplete');
 					
 				}, function errorCallback(response){
 					// We have our results already
 					$scope.suggestions = suggestions;
+					$scope.$broadcast('scroll.refreshComplete');
 				}
 			);
 		});
+    }
+	
+	auth.getUser().then(function(response){
+		
+		user = response.data;
+		$scope.user = user;
+		
+		$scope.loadSuggestions(user);
+		
+		// Send request
+		$scope.sendRequest = function(id, index){
+			
+			$scope.suggestions[index].loading = 1;
+			
+			$http.post(ApiData.url+'/connections/new/'+id, user, {
+				headers: {Authorization: 'Bearer '+auth.getToken()}
+			}).then(function successCallback(response) {
+				data = response.data;
+				
+				$scope.suggestions[index].loading = 2;
+
+			}, function errorCallback(response) {
+				data = response.data;
+				
+				$scope.showAlert(data);
+				
+				$scope.suggestions[index].loading = 0;
+			});
+		};
 	});
 })
